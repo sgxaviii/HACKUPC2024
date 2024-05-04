f1c7af87d645daeac398d59e0348404ba01aa092
miquel <miquelbaztan@gmail.com>
2024-05-04 16:47:26 +0200

    Posem els HH

---
+++ PlatformIO/Projects/HackUPC 2024/config.h
@@ -0,0 +1,16 @@
+#define WIFI_SSID     "HCKUPC2024" // Add wifi name
+#define WIFI_PASSWORD "Biene2024!" // Add wifi passowrd
+
+#define ID "super sensor" // Add unique name for this sensor
+#define INTERVAL 5 //seconds
+
+#define ONE_WIRE_BUS 16
+
+#define DHTPIN 4    // Which pin is DHT 11 connected to
+#define DHTTYPE DHT11 // Type DHT 11
+
+#define GC_PORT 443
+#define GC_PROM_URL "prometheus-us-central1.grafana.net"
+#define GC_PROM_PATH "/api/prom/push"
+#define GC_PROM_USER ""
+#define GC_PROM_PASS ""
